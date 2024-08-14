665cfa02325f178836c073ea77c932ec93181cec
VeronikaTereshchenko <verteresh@gmail.com>
2024-08-14 19:23:48 +0300

    OOP 1st version

---
+++ Exceptions/BankOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditInBank.Exceptions
+{
+    internal class BankOperation
+    {
+        public class InvalidBankOperationExeption : InvalidOperationException
+        {
+            public InvalidBankOperationExeption(string message) : base(message)
+            {
+            }
+        }
+    }
+}
